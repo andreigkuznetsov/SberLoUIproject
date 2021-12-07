40433bfd3367f5006b50e76c4f2c9b4b6d998d57
andreikuz <andreikuz@yahoo.com>
2021-12-07 18:33:39 +0300

    fifth commit

--- src/test/java/yahoo/andreikuzn/tests/WebpagesAboutDpdTests.java
+++ src/test/java/yahoo/andreikuzn/tests/WebpagesAboutDpdTests.java
@@ -1,6 +1,5 @@
 package yahoo.andreikuzn.tests;
 
-import io.qameta.allure.Feature;
 import io.qameta.allure.Owner;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -11,39 +10,90 @@ public class WebpagesAboutDpdTests extends TestBase {
 
     @Test
     @Owner("Andrei Kuznetsov")
-    @Feature("DPD.ru")
-    @DisplayName("Проверка секции О Компании сайта DPD.ru")
-    void aboutDpdWebsiteTests() throws IOException {
-
+    @DisplayName("Проверка страницы О Компании сайта DPD.ru")
+    void aboutDpdPage() {
         aboutCompanyPage.openAboutPage()
                 .checkAboutText()
-                .checkAboutBreadcrumbs()
-                .openMissionPage()
-                .checkMissionText()
-                .checkMissionBreadcrumbs()
-                .openHistoryPage()
-                .checkHistoryText()
-                .checkHistoryBreadcrumbs()
-                .openFactsPage()
-                .checkFactsText()
-                .checkFactsBreadcrumbs()
-                .openSuccessPage()
-                .checkSuccessText()
-                .checkSuccessBreadcrumbs()
-                .openManagementPage()
-                .checkManagementText()
-                .checkManagementBreadcrumbs()
-                .openLisencePage()
-                .checkLisenceText()
-                .checkLisenceBreadcrumbs()
-                .openAwardsPage()
-                .checkAwardsText()
-                .checkAwardsBreadcrumbs()
-                .openQualityPage()
-                .checkQualityText()
-                .checkQualityBreadcrumbs()
-                .checkQualityPolicyDoc();
+                .checkAboutBreadcrumbs();
+    }
 
+    @Test
+    @Owner("Andrei Kuznetsov")
+    @DisplayName("Проверка страницы Миссия секции О Компании сайта DPD.ru")
+    void missionDpdPage() {
+        aboutCompanyPage.openMissionPage()
+                .checkMissionText()
+                .checkMissionBreadcrumbs();
+    }
+
+    @Test
+    @Owner("Andrei Kuznetsov")
+    @DisplayName("Проверка страницы История секции О Компании сайта DPD.ru")
+    void historyDpdPage() {
+        aboutCompanyPage.openHistoryPage()
+                .checkHistoryText()
+                .checkHistoryBreadcrumbs();
+    }
+
+    @Test
+    @Owner("Andrei Kuznetsov")
+    @DisplayName("Проверка страницы Факты и цифры секции О Компании сайта DPD.ru")
+    void factsDpdPage() {
+        aboutCompanyPage.openFactsPage()
+                .checkFactsText()
+                .checkFactsBreadcrumbs();
+    }
+
+    @Test
+    @Owner("Andrei Kuznetsov")
+    @DisplayName("Проверка страницы Итории успеха секции О Компании сайта DPD.ru")
+    void successDpdPage() {
+        aboutCompanyPage.openSuccessPage()
+                .checkSuccessText()
+                .checkSuccessBreadcrumbs();
+    }
+
+    @Test
+    @Owner("Andrei Kuznetsov")
+    @DisplayName("Проверка страницы Управление секции О Компании сайта DPD.ru")
+    void managementDpdPage() {
+        aboutCompanyPage.openManagementPage()
+                .checkManagementText()
+                .checkManagementBreadcrumbs();
+    }
+
+    @Test
+    @Owner("Andrei Kuznetsov")
+    @DisplayName("Проверка страницы Лицензии секции О Компании сайта DPD.ru")
+    void licenseDpdPage() {
+        aboutCompanyPage.openLisencePage()
+                .checkLisenceText()
+                .checkLisenceBreadcrumbs();
+    }
+
+    @Test
+    @Owner("Andrei Kuznetsov")
+    @DisplayName("Проверка старницы Награды секции О Компании сайта DPD.ru")
+    void awardsDpdPage() {
+        aboutCompanyPage.openAwardsPage()
+                .checkAwardsText()
+                .checkAwardsBreadcrumbs();
+    }
+
+    @Test
+    @Owner("Andrei Kuznetsov")
+    @DisplayName("Проверка страницы Политика качества секции О Компании сайта DPD.ru")
+    void qualityDpdPage() {
+        aboutCompanyPage.openQualityPage()
+                .checkQualityText()
+                .checkQualityBreadcrumbs();
+    }
+
+    @Test
+    @Owner("Andrei Kuznetsov")
+    @DisplayName("Проверка PDF-файла Политика качества секции О Компании сайта DPD.ru")
+    void qualityPolicyPdf() throws IOException {
+        aboutCompanyPage.checkQualityPolicyDoc();
     }
 }
 
